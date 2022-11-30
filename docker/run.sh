93b48844fc3830b0a3f017a4ffffd1971c6cfa2a
derekray311511 <84118285+derekray311511@users.noreply.github.com>
2022-11-30 19:46:15 +0800

    Add files via upload

---
+++ docker/run.sh
@@ -0,0 +1,14 @@
+xhost +local:
+
+docker run \
+-it \
+--env="DISPLAY" \
+--env="QT_X11_NO_MITSHM=1" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--rm \
+--name tracking \
+-e GRANT_SUDO=yes \
+-v /data/Nuscenes/CBMOT/data:/data \
+-v /data/Nuscenes/CBMOT_update:/home/Student/Tracking \
+tracking \
+bash
