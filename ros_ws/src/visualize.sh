a1fba414e9bf0a3cc73acb1d5a934cbf74bb8af8
derekray311511 <84118285+derekray311511@users.noreply.github.com>
2022-12-06 19:19:29 +0800

    Update visualize.sh

--- ros_ws/src/visualize.sh
+++ ros_ws/src/visualize.sh
@@ -1,4 +1,4 @@
 detection_path="/data/detection_result.json"
-tracking_path="/data/track_results/CenterPoint/tracking_result.json"
+tracking_path="/data/track_results/tracking_result.json"
 vis_th=0.0
 python visualize.py --detection_path $detection_path --track_res_path $tracking_path --vis_th $vis_th
