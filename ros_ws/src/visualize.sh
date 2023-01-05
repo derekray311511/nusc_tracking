1cc175749daa75b595a765bed342ba9ecacc81de
derekray311511 <84118285+derekray311511@users.noreply.github.com>
2023-01-05 20:35:43 +0800

    Update visualize.sh

--- ros_ws/src/visualize.sh
+++ ros_ws/src/visualize.sh
@@ -1,7 +1,16 @@
 detection_path="/data/detection_result.json"
 tracking_path="/data/track_results/tracking_result.json"
-vis_th=0.0
+
+vis_th=0.05
 vis_img_bbox=1
-init_idx=1000
-python visualize.py --detection_path $detection_path --track_res_path $tracking_path --vis_th $vis_th --init_idx $init_idx \
---vis_img_bbox $vis_img_bbox
+
+lidar_stack=1
+radar_stack=1
+det_bbox_stack=1
+trk_bbox_stack=5
+init_idx=0
+
+python temp.py --detection_path $detection_path --track_res_path $tracking_path \
+--vis_th $vis_th --init_idx $init_idx --vis_img_bbox $vis_img_bbox \
+--lidar_stack $lidar_stack --radar_stack $radar_stack \
+--det_bbox_stack $det_bbox_stack --trk_bbox_stack $trk_bbox_stack
