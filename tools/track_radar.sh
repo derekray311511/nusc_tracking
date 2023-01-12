3de444f7c3e651d08a805a5502f1ed7472437ccf
derekray311511 <84118285+derekray311511@users.noreply.github.com>
2023-01-12 15:49:20 +0800

    Update track_radar.sh

--- tools/track_radar.sh
+++ tools/track_radar.sh
@@ -20,13 +20,14 @@ datetime="2023-01-11-19:18:23"
 evaluate=1
 out_dir="data/track_results/BEVFusion-KF"
 dataroot="data/nuscenes"
+workspace="/home/Student/Tracking"
 split="val"
 detection_path="data/track_result_bboxth-0.0/detection_result.json"
 # detection_path="data/detection_result.json"   # CenterPoint
 frames_meta_path="data/frames_meta.json"
 radar_tracker_path="data/radar_PC/$datetime/radar_tracking_result_13Hz.json"
 
-python tools/track_radar.py --split $split --out-dir $out_dir \
+python tools/track_radar.py --split $split --workspace $workspace --out-dir $out_dir \
 --min_hits $min_hits --det_th $det_th --del_th $del_th --active_th $active_th \
 --update_function $update_function --score_decay $score_decay \
 --evaluate $evaluate --dataroot $dataroot \
