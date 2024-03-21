1eb9a2c7fc374873eab97f13d666d7c38ab5a759
derekray311511 <derekray311511@gmail.com>
2024-03-21 17:01:15 +0800

    Add visualization for TP, FP, FN

--- tools/viz_res.py
+++ tools/viz_res.py
@@ -30,7 +30,7 @@ from tracker import PubTracker as lidarTracker
 from early_fusion_tracker import PubTracker as radarTracker
 from early_fusion_fusion import Fusion
 
-from utils.custom_eval import evaluate_nuscenes, draw_boxes
+from utils.custom_eval import evaluate_nuscenes, TrackingEvaluation
 
 class res_data:
     def __init__(
@@ -78,36 +78,74 @@ def setWinPos(screenSize, winList):
             w, h = win.windowSize
             cv2.moveWindow(win.windowName, window_width - int(w/2), (i-2) * h)
 
-def analyze_res(
-    trk1=None, 
-    trk2=None, 
-    radarSeg=None, 
-    radarTrk=None, 
-    gt=None, 
-    LiDAR_PC=None,
-    image=None,
-    ):
-    # Show the numbers of targets in each frame with categories
-    if trk1 is not None and trk2 is not None:
-        trk1_categories = [obj['tracking_name'] for obj in trk1]
-        trk2_categories = [obj['tracking_name'] for obj in trk2]
-        trk1_category_counts = {category: trk1_categories.count(category) for category in set(trk1_categories)}
-        trk2_category_counts = {category: trk2_categories.count(category) for category in set(trk2_categories)}
-        print("trk1 len:", len(trk1),", trk2 len:", len(trk2))
-        print("trk1:", trk1_category_counts); print("trk2: ", trk2_category_counts)
-        # Show gt numbers of targets in each frame with categories
-        if gt is None:
-            pass
-        gt_categories = [obj['detection_name'] for obj in gt]
-        gt_category_counts = {category: gt_categories.count(category) for category in set(gt_categories)}
-        print("gt:", len(gt))
-        print("gt:", gt_category_counts)
+class TrackEval(object):
+    def __init__(self):
+        self.trackEval_1 = TrackingEvaluation()
+        self.trackEval_2 = TrackingEvaluation()
+        self.total_data = {'trk1': [0, 0, 0], 'trk2': [0, 0, 0]}
 
-        # Calculate TP, FP, FN in each frame
-        eval_trk1 = (TP, FP, FN, matched_predictions) = evaluate_nuscenes(trk1, gt)
-        print("trk1 FP, FP, FN: ", eval_trk1[:3])
-        eval_trk2 = (TP, FP, FN, matched_predictions) = evaluate_nuscenes(trk2, gt)
-        print("trk2 FP, FP, FN: ", eval_trk2[:3])
+    def get_matched_objects(self, matched_ids, objects):
+        """
+        Extract objects matching the provided IDs from a list of objects.
+        """
+        if len(objects) == 0:
+            return []
+        if 'tracking_id' in objects[0]:
+            return [obj for obj in objects if obj['tracking_id'] in matched_ids]
+        else:
+            return [obj for obj in objects if obj['instance_token'] in matched_ids]
+
+    def analyze_res(self, trk1=None, trk2=None, gt=None, accumulate=True):
+        # Show the numbers of targets in each frame with categories
+        if trk1 is not None and trk2 is not None:
+            trk1_categories = [obj['tracking_name'] for obj in trk1]
+            trk2_categories = [obj['tracking_name'] for obj in trk2]
+            trk1_category_counts = {category: trk1_categories.count(category) for category in set(trk1_categories)}
+            trk2_category_counts = {category: trk2_categories.count(category) for category in set(trk2_categories)}
+            print("trk1 len:", len(trk1),", trk2 len:", len(trk2))
+            print("trk1:", trk1_category_counts); print("trk2: ", trk2_category_counts)
+            # Show gt numbers of targets in each frame with categories
+            if gt is None:
+                pass
+            gt_categories = [obj['detection_name'] for obj in gt]
+            gt_category_counts = {category: gt_categories.count(category) for category in set(gt_categories)}
+            print("gt:", len(gt))
+            print("gt:", gt_category_counts)
+
+            # Calculate TP, FP, FN in each frame
+            mota_1, _ = self.trackEval_1.evaluate_nuscenes_mota(trk1, gt, distance_threshold=3.0)
+            mota_2, _ = self.trackEval_2.evaluate_nuscenes_mota(trk2, gt, distance_threshold=3.0)
+            events = mota_1.events
+            TP = len(events[events['Type'] == 'MATCH'])
+            FP = len(events[events['Type'] == 'FP'])
+            FN = len(events[events['Type'] == 'MISS'])
+            if accumulate:
+                self.total_data['trk1'] += np.array([TP, FP, FN])
+            print("total trk1 TP, FP, FN:", self.total_data['trk1'])
+            print("trk1 TP, FP, FN:", TP, FP, FN)
+            events = mota_2.events
+            TP = len(events[events['Type'] == 'MATCH'])
+            FP = len(events[events['Type'] == 'FP'])
+            FN = len(events[events['Type'] == 'MISS'])
+            if accumulate:
+                self.total_data['trk2'] += np.array([TP, FP, FN])
+            print("total trk2 TP, FP, FN:", self.total_data['trk2'])
+            print("trk2 TP, FP, FN:", TP, FP, FN)
+
+            matched_pred_ids_trk1 = mota_1.events[mota_1.events['Type'] == 'MATCH']['HId'].unique()
+            matched_gt_ids_trk1 = mota_1.events[mota_1.events['Type'] == 'MATCH']['OId'].unique()
+            matched_pred_trk1 = self.get_matched_objects(matched_pred_ids_trk1, trk1)
+            matched_gt_trk1 = self.get_matched_objects(matched_gt_ids_trk1, gt)
+
+            matched_pred_ids_trk2 = mota_2.events[mota_2.events['Type'] == 'MATCH']['HId'].unique()
+            matched_gt_ids_trk2 = mota_2.events[mota_2.events['Type'] == 'MATCH']['OId'].unique()
+            matched_pred_trk2 = self.get_matched_objects(matched_pred_ids_trk2, trk2)
+            matched_gt_trk2 = self.get_matched_objects(matched_gt_ids_trk2, gt)
+
+            mota_1.reset()
+            mota_2.reset()
+
+        return (matched_pred_trk1, matched_gt_trk1), (matched_pred_trk2, matched_gt_trk2)
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -137,6 +175,7 @@ def main(parser) -> None:
         radarSeg_path=f"/data/early_fusion_track_results/{version}/radar_seg_res.json",
         radarTrk_path=f"/data/early_fusion_track_results/{version}/radar_tracking_res.json",
     )
+    trackEval = TrackEval()
     winName1 = 'LiDAR'
     winName2 = 'Fusion'
     cfg["VISUALIZER"]["trk_res"] = {
@@ -158,21 +197,30 @@ def main(parser) -> None:
         windowName='radarTrk',
         **cfg["VISUALIZER"],
     )
-    winList = [trackViz, trackViz2, trackViz3]
+    analyzeWin1 = TrackVisualizer(
+        windowName='analyzeTrk1',
+        **cfg["VISUALIZER"],
+    )
+    analyzeWin2 = TrackVisualizer(
+        windowName='analyzeTrk2',
+        **cfg["VISUALIZER"],
+    )
+    winList = [trackViz, trackViz2, trackViz3, analyzeWin1, analyzeWin2]
     setWinPos(cfg["VISUALIZER"]["screenSize"], winList)
 
     frames = dataset.get_frames_meta()
     gts = dataset.get_groundTruth()
+    idx_record = set()
     for gt in gts:
         for obj in gt['anns']:
             obj['velocity'] = np.array([0.0, 0.0])
     len_frames = len(frames)
     max_idx = len_frames - 1
     idx = -1
-    cv2.createTrackbar('Frame', winName1, 0, max_idx, lambda x: None)
-    cv2.createTrackbar('Frame', winName2, 0, max_idx, lambda x: None)
-    cv2.setTrackbarPos('Frame', winName1, idx)
-    cv2.setTrackbarPos('Frame', winName2, idx)
+    for win in winList:
+        cv2.createTrackbar('Frame', win.windowName, 0, max_idx, lambda x: None)
+        cv2.setTrackbarPos('Frame', win.windowName, idx)
+        
     while True:
 
         if trackViz.play:
@@ -197,20 +245,21 @@ def main(parser) -> None:
             trackViz.duration *= 0.5
             print(f"Viz duration set to {trackViz.duration}")
         elif key == ord('g'):
-            trackViz.grid = not trackViz.grid
-            trackViz2.grid = not trackViz2.grid
+            for win in winList:
+                win.grid = not win.grid
         elif key == 13:
-            new_idx1 = cv2.getTrackbarPos('Frame', winName1)
-            new_idx2 = cv2.getTrackbarPos('Frame', winName2)
-            idx = new_idx1 if new_idx1 != idx else new_idx2
+            for win in winList:
+                winName = win.windowName
+                det_idx = cv2.getTrackbarPos('Frame', winName)
+                idx = det_idx if det_idx != idx else idx
 
         if idx < 0:
             idx = 0
         if idx >= len_frames:
             idx = len_frames - 1
 
-        cv2.setTrackbarPos('Frame', winName1, idx)
-        cv2.setTrackbarPos('Frame', winName2, idx)
+        for win in winList:
+            cv2.setTrackbarPos('Frame', win.windowName, idx)
 
         token = frames[idx]['token']
         timestamp = frames[idx]['timestamp']
@@ -221,6 +270,13 @@ def main(parser) -> None:
         radarTrk = trk_res.radarTrk[token]
         gt = gts[idx]['anns']
         frame_name = "{}-{}".format(timestamp, token)
+
+        if idx not in idx_record:
+            accumulate = True
+        else:
+            accumulate = False
+        idx_record.add(idx)
+        eval_trk1, eval_trk2 = trackEval.analyze_res(trk1, trk2, gt, accumulate=accumulate)
 
         trans = dataset.get_4f_transform(ego_pose, inverse=True)
         viz_start = time.time()
@@ -235,15 +291,20 @@ def main(parser) -> None:
         trackViz3.draw_ego_car(img_src="/data/car1.png")
         trackViz3.draw_radar_seg(radarSeg, trans, **cfg["VISUALIZER"]["radarSeg"])
         trackViz3.draw_det_bboxes(radarTrk, trans, **cfg["VISUALIZER"]["radarTrkBox"])
+        analyzeWin1.draw_ego_car(img_src="/data/car1.png")
+        analyzeWin1.draw_det_bboxes(gt, trans, **cfg["VISUALIZER"]["groundTruth"])
+        analyzeWin1.drawTP_FP_FN(trk1, gt, eval_trk1[0], eval_trk1[1], trans)
+        analyzeWin2.draw_ego_car(img_src="/data/car1.png")
+        analyzeWin2.draw_det_bboxes(gt, trans, **cfg["VISUALIZER"]["groundTruth"])
+        analyzeWin2.drawTP_FP_FN(trk2, gt, eval_trk2[0], eval_trk2[1], trans)
+        analyzeWin1.show()
+        analyzeWin2.show()
         trackViz.show()
         trackViz2.show()
         trackViz3.show()
         viz_end = time.time()
         if args.show_delay:
             print(f"viz delay:{(viz_end - viz_start) / 1e-3: .2f} ms")
-
-        analyze_res(trk1, trk2, radarSeg, radarTrk, gt, None, 
-                    image=np.ones((trackViz.height, trackViz.width, 3), dtype=np.uint8) * trackViz.background_color)
 
         print()
         
