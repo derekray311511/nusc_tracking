8d38dd3b73c430a7d44820155a01be8319f9ec77
derekray311511 <derekray311511@gmail.com>
2024-03-21 15:03:57 +0800

    Add util for calculate TP, FP, FN (same as nusc)

--- tools/utils/custom_eval.py
+++ tools/utils/custom_eval.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import sklearn
+from nuscenes.eval.tracking.mot import MOTAccumulatorCustom
 
 def calculate_distance(gt_translation, pred_translation):
     return np.linalg.norm(np.array(gt_translation) - np.array(pred_translation))
@@ -9,6 +11,7 @@ def evaluate_nuscenes(predictions, ground_truths, distance_threshold=2.0):
     FP = 0
     FN = 0
     matched_predictions = []
+    matched_gt = []
 
     for gt in ground_truths:
         matched = False
@@ -18,55 +21,57 @@ def evaluate_nuscenes(predictions, ground_truths, distance_threshold=2.0):
                 matched = True
                 if pred not in matched_predictions:
                     TP += 1
+                    matched_gt.append(gt)
                     matched_predictions.append(pred)
                 break
         if not matched:
             FN += 1
 
     FP = len(predictions) - len(matched_predictions)
-    return TP, FP, FN, matched_predictions
+    return TP, FP, FN, matched_predictions, matched_gt
 
 # Assume `predictions` and `ground_truths` are lists of dictionaries with 'translation', 'size', 'velocity'
 
 
-def draw_boxes(image, predictions, ground_truths, matched_predictions):
-    if image is None:
-        return
-    # Draw FN boxes in blue
-    for gt in ground_truths:
-        if gt not in matched_predictions:
-            top_left = (int(gt['translation'][0] - gt['size'][0]/2), int(gt['translation'][1] - gt['size'][1]/2))
-            bottom_right = (int(gt['translation'][0] + gt['size'][0]/2), int(gt['translation'][1] + gt['size'][1]/2))
-            cv2.rectangle(image, top_left, bottom_right, (255, 0, 0), 2)
-    
-    # Draw TP and FP boxes
-    for pred in predictions:
-        top_left = (int(pred['translation'][0] - pred['size'][0]/2), int(pred['translation'][1] - pred['size'][1]/2))
-        bottom_right = (int(pred['translation'][0] + pred['size'][0]/2), int(pred['translation'][1] + pred['size'][1]/2))
-        color = (0, 255, 0) if pred in matched_predictions else (0, 0, 255)
-        cv2.rectangle(image, top_left, bottom_right, color, 2)
+class TrackingEvaluation(object):
+    def __init__(self):
+        self.motaAccumulator = MOTAccumulatorCustom()   # one for each scene
+        self.frame_id = 0
 
-# `image` is your image on which you want to draw
-# Make sure to adjust the bounding box drawing logic if your 'translation' represents the center.
+    def evaluate_nuscenes_mota(self, frame_pred, frame_gt, distance_threshold=2.0):
+        """
+        Evaluate the tracking results using the MOTA metric.
+        """
+        mota = self.motaAccumulator
+        
+        # Abort if there are neither GT nor pred boxes.
+        # print(frame_gt[0].keys())
+        # print(frame_pred[0].keys())
+        gt_ids = [gg['instance_token'] for gg in frame_gt]
+        pred_ids = [tt['tracking_id'] for tt in frame_pred]
+        if len(gt_ids) == 0 and len(pred_ids) == 0:
+            return None, []
 
-def test():
-    predictions = [
-        {'translation': [5, 5], 'size': [2, 2], 'velocity': [0, 0]},
-        {'translation': [10, 10], 'size': [2, 2], 'velocity': [0, 0]},
-        {'translation': [3, 3], 'size': [2, 2], 'velocity': [0, 0]},
-    ]
-    ground_truths = [
-        {'translation': [5, 5], 'size': [2, 2], 'velocity': [0, 0]},
-        {'translation': [10, 10], 'size': [2, 2], 'velocity': [0, 0]},
-        {'translation': [4, 4], 'size': [2, 2], 'velocity': [0, 0]},
-        {'translation': [15, 15], 'size': [2, 2], 'velocity': [0, 0]},
-    ]
-    TP, FP, FN, matched_predictions = evaluate_nuscenes(predictions, ground_truths)
-    print(TP, FP, FN)
-    image = np.zeros((20, 20, 3), dtype=np.uint8)
-    draw_boxes(image, predictions, ground_truths, matched_predictions)
-    cv2.imshow('image', image)
-    cv2.waitKey(0)
+        if len(frame_gt) == 0 or len(frame_pred) == 0:
+            distances = np.ones((0, 0))
+        else:
+            gt_boxes = np.array([b['translation'][:2] for b in frame_gt])
+            pred_boxes = np.array([b['translation'][:2] for b in frame_pred])
+            distances = sklearn.metrics.pairwise.euclidean_distances(gt_boxes, pred_boxes)
 
-if __name__ == "__main__":
-    test()
+        # Distances that are larger than the threshold won't be associated.
+        assert len(distances) == 0 or not np.all(np.isnan(distances))
+        distances[distances >= distance_threshold] = np.nan
+
+        # Accumulate results.
+        # Note that we cannot use timestamp as frameid as motmetrics assumes it's an integer.
+        mota.update(gt_ids, pred_ids, distances, frameid=self.frame_id)
+
+        events = mota.events.loc[self.frame_id]
+        matches = events[events.Type == 'MATCH']
+        match_ids = matches.HId.values
+        match_scores = [tt['tracking_score'] for tt in frame_pred if tt['tracking_id'] in match_ids]
+
+        self.frame_id += 1
+
+        return mota, match_scores
